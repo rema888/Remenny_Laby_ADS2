8e963c58c3c888b774f4713456a82882a9e4c7ce
rema888 <aremenny@gmail.com>
2026-05-09 15:36:03 +0200

    Add: 5 лабораторная работа

--- Laba_5/ads2.5.py
+++ Laba_5/ads2.5.py
@@ -1,37 +1,23 @@
-def get_substring_bm(text, pattern):
-    result = []
+def bm(text, pattern):
     n, m = len(text), len(pattern)
+    # Создаем словарь только для тех букв, что есть в шаблоне
+    skip = {c: i for i, c in enumerate(pattern)}
+    res = []
+    i = 0
 
-    if m == 0 or m > n:
-        return result
-
-    bad_char = {}
-    for i in range(m):
-        bad_char[pattern[i]] = i
-
-    shift = 0
-    while shift <= n - m:
+    while i <= n - m:
         j = m - 1
 
-        while j >= 0 and pattern[j] == text[shift + j]:
+        while j >= 0 and pattern[j] == text[i + j]:
             j -= 1
 
         if j < 0:
-
-            result.append(shift)
-
-            if shift + m < n:
-                char_in_text = text[shift + m]
-                shift += m - bad_char.get(char_in_text, -1)
-            else:
-                shift += 1
+            res.append(i)
+            i += 1
         else:
-
-            char_in_text = text[shift + j]
-            shift += max(1, j - bad_char.get(char_in_text, -1))
-
-    return result
+            i += max(1, j - skip.get(text[i + j], -1))
+    return res
 
 text = "однажды в студеную зимнюю пору"
 pattern = "пору"
-print(f"Найдено на позициях: {get_substring_bm(text, pattern)}")
+print(f"Найдено на позициях: {bm(text, pattern)}")
